c84fa044a8d1ab9899cc27fde1622ed937317482
Leon <leonzheng66@gmail.com>
2023-02-19 01:25:31 -0500

    add dockerfile

--- src/main/java/com/zheng/zeninventory/model/CustomerOrder.java
+++ src/main/java/com/zheng/zeninventory/model/CustomerOrder.java
@@ -4,11 +4,15 @@ import java.io.Serializable;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -34,16 +38,20 @@ public class CustomerOrder extends Order implements Serializable{
 	
     @ManyToOne(cascade = {
             CascadeType.PERSIST,
-            CascadeType.MERGE
+            CascadeType.MERGE,
+            CascadeType.REFRESH
         })
     @JoinColumn(name = "customer_id")
+    @NotFound(action = NotFoundAction.IGNORE)
 	private Customer customer;
 	
     @ManyToOne(cascade = {
             CascadeType.PERSIST,
-            CascadeType.MERGE
+            CascadeType.MERGE,
+            CascadeType.REFRESH
         })
     @JoinColumn(name = "product_id")
+    @NotFound(action = NotFoundAction.IGNORE)
 	private Product product;
 	private int purchaseQuantity;
 	private double purchasePrice;
